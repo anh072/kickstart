8d9c31c9118673cf49f2270f65270861e95074d3
Anh Nguyen <anhnguyen@contino.io>
2020-05-31 12:05:48 +1000

    refactored the code for 2020 C c

--- 2020/C/candies.py
+++ 2020/C/candies.py
@@ -1,42 +1,36 @@
 # logn for update
-def update1(val, i, left, right, tree, index):
+def update(mode, val, i, left, right, tree, index):
     if i < left or i > right:
         return
     if left == right:
-        tree[index] = (-1)**i * val
+        if mode == "S":
+            tree[index] = (-1)**i * val
+        else:
+            tree[index] = (-1)**i * val * (i+1)
         return
     mid = left + (right-left)//2
-    update1(val, i, left, mid, tree, 2*index+1)
-    update1(val, i, mid+1, right, tree, 2*index+2)
+    update(mode, val, i, left, mid, tree, 2*index+1)
+    update(mode, val, i, mid+1, right, tree, 2*index+2)
     tree[index] = tree[2*index+1] + tree[2*index+2]
 
 
-# logn for update
-def update2(val, i, left, right, tree, index):
-    if i < left or i > right:
+def build_util(mode, tree, ss, se, arr, si):
+    if ss == se:
+        if mode == "S":
+            tree[si] = (-1)**ss * arr[ss]
+        else:
+            tree[si] = (-1)**ss * arr[ss] * (ss+1)
         return
-    if left == right:
-        tree[index] = (-1)**i * val * (i+1)
-        return
-    mid = left + (right-left)//2
-    update2(val, i, left, mid, tree, 2*index+1)
-    update2(val, i, mid+1, right, tree, 2*index+2)
-    tree[index] = tree[2*index+1] + tree[2*index+2]
+    mid = ss + (se-ss)//2
+    build_util(mode, tree, ss, mid, arr, 2*si+1)
+    build_util(mode, tree, mid+1, se, arr, 2*si+2)
+    tree[si] = tree[2*si+1] + tree[2*si+2]
 
 
-# nlogn to build
-def build1(arr, N):
+# O(n) to build
+def build(arr, N, mode):
     tree = [0] * (2*N-1)
-    for i, val in enumerate(arr):
-        update1(val, i, 0, N-1, tree, 0)
-    return tree
-
-
-# nlogn to build
-def build2(arr, N):
-    tree = [0] * (2*N-1)
-    for i, val in enumerate(arr):
-        update2(val, i, 0, N-1, tree, 0)
+    build_util(mode, tree, 0, N-1, arr, 0)
     return tree
 
 
@@ -62,9 +56,9 @@ def solve():
     N, Q = map(int, input().split())
     candies = [int(c) for c in input().split() ]
     # construct a segment tree (-1)^i A(i)
-    tree1 = build1(candies, N)
+    tree1 = build(candies, N, "S")
     # construct a segment tree (-1)^i A(i)*i
-    tree2 = build2(candies, N)
+    tree2 = build(candies, N, "M")
     ans = 0
     for i in range(Q):
         q, a, b = input().split()
@@ -72,8 +66,8 @@ def solve():
         if q == "Q":
             ans += query(tree1, tree2, a-1, b-1, N)
         elif q == "U":
-            update1(b, a-1, 0, N-1, tree1, 0)
-            update2(b, a-1, 0, N-1, tree2, 0)
+            update("S", b, a-1, 0, N-1, tree1, 0)
+            update("M", b, a-1, 0, N-1, tree2, 0)
     return ans
 
 T = int(input())
